fe63cc5b001bbdd7d410104b1c5fcc4131a9402f
Clint Plummer <clint@yuhonas.com>
2018-12-16 20:19:56 -0800

    initial project commit

---
+++ lib/quaidcanhelp.ex
@@ -0,0 +1,18 @@
+defmodule Quaidcanhelp do
+  @moduledoc """
+  Documentation for Quaidcanhelp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quaidcanhelp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quaidcanhelp_test.exs
@@ -0,0 +1,8 @@
+defmodule QuaidcanhelpTest do
+  use ExUnit.Case
+  doctest Quaidcanhelp
+
+  test "greets the world" do
+    assert Quaidcanhelp.hello() == :world
+  end
+end
